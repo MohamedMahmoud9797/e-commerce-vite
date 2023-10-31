f5b396ebf3fa8b3607e4d6e0b26aabf6254d6503
Your Name <you@example.com>
2023-11-01 00:11:45 +0200

    Add left section on header 3

--- my-react-app/src/components/header/Header3.jsx
+++ my-react-app/src/components/header/Header3.jsx
@@ -1,3 +1,121 @@
-export default function Header3() {
-  return <div>header 3</div>;
-}
+import {
+  Box,
+  Container,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+
+import WindowIcon from "@mui/icons-material/Window";
+import KeyboardArrowRightOutlinedIcon from "@mui/icons-material/KeyboardArrowRightOutlined";
+import {
+  SportsEsportsOutlined,
+  ElectricBikeOutlined,
+  LaptopChromebookOutlined,
+  MenuBookOutlined,
+} from "@mui/icons-material";
+
+const Header3 = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const theme = useTheme();
+
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        mt: 5,
+      }}
+    >
+      <Box>
+        <Button
+          id="basic-button"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+          sx={{
+            width: 222,
+            color: theme.palette.text.secondary,
+          }}
+        >
+          <WindowIcon />
+          <Typography
+            sx={{
+              padding: "0",
+              textTransform: "capitalize",
+              mx: 1,
+            }}
+          >
+            Categories
+          </Typography>
+          <Box flexGrow={1} />
+
+          <KeyboardArrowRightOutlinedIcon />
+        </Button>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+          sx={{
+            ".MuiPaper-root": {
+              width: 220,
+            },
+          }}
+        >
+          <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+              <ElectricBikeOutlined fontSize="small" />
+            </ListItemIcon>
+
+            <ListItemText>Bikes</ListItemText>
+          </MenuItem>
+
+          <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+              <LaptopChromebookOutlined fontSize="small" />
+            </ListItemIcon>
+
+            <ListItemText>Electronics</ListItemText>
+          </MenuItem>
+
+          <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+              <MenuBookOutlined fontSize="small" />
+            </ListItemIcon>
+
+            <ListItemText>Books</ListItemText>
+          </MenuItem>
+
+          <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+              <SportsEsportsOutlined fontSize="small" />
+            </ListItemIcon>
+
+            <ListItemText>Games</ListItemText>
+          </MenuItem>
+        </Menu>
+      </Box>
+    </Container>
+  );
+};
+
+export default Header3;
