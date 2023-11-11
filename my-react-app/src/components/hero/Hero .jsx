e4761720e92431fc567e37481fbb01e49987f25c
Your Name <you@example.com>
2023-11-11 23:27:52 +0200

    s

--- my-react-app/src/components/hero/Hero .jsx
+++ my-react-app/src/components/hero/Hero .jsx
@@ -4,8 +4,16 @@ import React from "react";
 
 // Import Swiper styles
 import { Swiper, SwiperSlide } from "swiper/react";
-import {  Pagination, } from "swiper/modules";
-import { Grid, Container, Box, Typography, Button, Stack, useTheme } from "@mui/material";
+import { Pagination } from "swiper/modules";
+import {
+  Grid,
+  Container,
+  Box,
+  Typography,
+  Button,
+  Stack,
+  useTheme,
+} from "@mui/material";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
@@ -13,9 +21,9 @@ import "./slider.css";
 const Hero = () => {
   const theme = useTheme();
   const sliderData = [
-    {text: "Men", image: "../../../public/assstes/banner-15.jpg"},
-    {text: "Women", image: "../../../public/assstes/banner-25.jpg"},
-  ]
+    { text: "Men", image: "../../../public/assstes/banner-15.jpg" },
+    { text: "Women", image: "../../../public/assstes/banner-25.jpg" },
+  ];
 
   return (
     <Box sx={{ backgroundColor: theme.palette.grey.main }}>
@@ -23,107 +31,105 @@ const Hero = () => {
         <Grid container spacing={2} direction={"row"} sx={{ mb: 5 }}>
           {/* left side grid item */}
           <Grid item md={8} xs={12}>
-          <Swiper
-          loop={true}
-          pagination={{
-            dynamicBullets: true,
-          }}
-          modules={[Pagination]}
-          className="mySwiper"
-        >
-          {sliderData.map((item) => {
-            return (
-              <SwiperSlide key={item.link} className="parent-slider">
-                <img src={item.image} alt="" />
-                <Box
-                  sx={{
-                    [theme.breakpoints.up("sm")]: {
-                      position: "absolute",
-                      left: "10%",
-                      textAlign: "left",
-                    },
+            <Swiper
+              loop={true}
+              pagination={{
+                dynamicBullets: true,
+              }}
+              modules={[Pagination]}
+              className="mySwiper"
+            >
+              {sliderData.map((item) => {
+                return (
+                  <SwiperSlide key={item.link} className="parent-slider">
+                    <img src={item.image} alt="" />
+                    <Box
+                      sx={{
+                        [theme.breakpoints.up("sm")]: {
+                          position: "absolute",
+                          left: "10%",
+                          textAlign: "left",
+                        },
 
-                    [theme.breakpoints.down("sm")]: {
-                      pt: 4,
-                      pb: 6,
-                    },
-                  }}
-                >
-                  <Typography
-                    sx={{
-                      color: "#222",
-                    }}
-                    variant="h5"
-                  >
-                    LIFESTYLE COLLECTION
-                  </Typography>
+                        [theme.breakpoints.down("sm")]: {
+                          pt: 4,
+                          pb: 6,
+                        },
+                      }}
+                    >
+                      <Typography
+                        sx={{
+                          color: "#222",
+                        }}
+                        variant="h5"
+                      >
+                        LIFESTYLE COLLECTION
+                      </Typography>
 
-                  <Typography
-                    sx={{
-                      color: "#222",
-                      fontWeight: 500,
-                      my: 1,
-                    }}
-                    variant="h3"
-                  >
-                    {item.text}
-                  </Typography>
+                      <Typography
+                        sx={{
+                          color: "#222",
+                          fontWeight: 500,
+                          my: 1,
+                        }}
+                        variant="h3"
+                      >
+                        {item.text}
+                      </Typography>
 
-                  <Stack
-                    sx={{
-                      justifyContent: { xs: "center", sm: "left" },
-                    }}
-                    direction={"row"}
-                    alignItems={"center"}
-                  >
-                    <Typography color={"#333"} mr={1} variant="h4">
-                      SALE UP TO
-                    </Typography>
-                    <Typography color={"#D23F57"} variant="h4">
-                      30% OFF
-                    </Typography>
-                  </Stack>
-                  <Typography
-                    sx={{
-                      color: "#000",
-                      fontWeight: 300,
-                      my: 1,
-                    }}
-                    variant="body1"
-                  >
-                    Get Free Shipping on orders over $99.00
-                  </Typography>
+                      <Stack
+                        sx={{
+                          justifyContent: { xs: "center", sm: "left" },
+                        }}
+                        direction={"row"}
+                        alignItems={"center"}
+                      >
+                        <Typography color={"#333"} mr={1} variant="h4">
+                          SALE UP TO
+                        </Typography>
+                        <Typography color={"#D23F57"} variant="h4">
+                          30% OFF
+                        </Typography>
+                      </Stack>
+                      <Typography
+                        sx={{
+                          color: "#000",
+                          fontWeight: 300,
+                          my: 1,
+                        }}
+                        variant="body1"
+                      >
+                        Get Free Shipping on orders over $99.00
+                      </Typography>
 
-                  <Button
-                    sx={{
-                      px: 5,
-                      py: 1,
-                      mt: 2,
-                      backgroundColor: "#222",
-                      boxShadow: "0px 4px 16px rgba(43, 52, 69, 0.1)",
-                      color: "#fff",
-                      borderRadius: "1px",
-                      "&:hover": {
-                        bgcolor: "#151515",
-                        boxShadow: "0px 4px 16px rgba(43, 52, 69, 0.1)",
-                      },
-                    }}
-                    variant="contained"
-                  >
-                    shop now
-                  </Button>
-                </Box>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-
+                      <Button
+                        sx={{
+                          px: 5,
+                          py: 1,
+                          mt: 2,
+                          backgroundColor: "#222",
+                          boxShadow: "0px 4px 16px rgba(43, 52, 69, 0.1)",
+                          color: "#fff",
+                          borderRadius: "1px",
+                          "&:hover": {
+                            bgcolor: "#151515",
+                            boxShadow: "0px 4px 16px rgba(43, 52, 69, 0.1)",
+                          },
+                        }}
+                        variant="contained"
+                      >
+                        shop now
+                      </Button>
+                    </Box>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
           </Grid>
 
           {/* right  side grid item */}
           <Grid item md={4}>
-            <Grid   
-            >
+            <Grid>
               <Box>
                 <img
                   src="../../../public/assstes/banner-17.jpg"
